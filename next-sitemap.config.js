881de3296ceb8b6c7b6b70b10cb3a9dab3f908bf
MattSeb15 <mateosebastian159@gmail.com>
2025-03-21 08:39:36 -0500

    Actualizar la URL del sitio en la configuración de sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	siteUrl: 'https://www.tusitio.com', // Reemplaza con la URL de tu sitio
+	siteUrl: 'https://most-studio.vercel.app/', // Reemplaza con la URL de tu sitio
 	generateRobotsTxt: true, // (opcional) Genera el archivo robots.txt
 	// Opciones adicionales
 }
